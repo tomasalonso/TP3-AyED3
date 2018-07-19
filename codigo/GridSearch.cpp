4cf2aa63f8dc39d66ab057b13215e9b2cbf47078
agrosso <agrosso@dc.uba.ar>
2018-07-19 17:12:08 -0300

    Grid search: otro error más arreglada

--- codigo/GridSearch.cpp
+++ codigo/GridSearch.cpp
@@ -52,7 +52,7 @@ Genoma generar() {
         cerr << rand_num << endl;
     }
     for (unsigned int j = inicio_probas; j < genoma_size; ++j) {
-        const double rand_num = distribucion1(generador);
+        const double rand_num = distribucion2(generador);
 
         random.push_back(rand_num);
         cerr << rand_num << endl;
